8976ac16f384af383d61767953b1d332ae796995
jamby77 <jamby77@gmail.com>
2025-10-28 21:12:56 +0200

    add and configure better-auth

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,14 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
 
-import db from '@astrojs/db';
+import db from "@astrojs/db";
 
 // https://astro.build/config
 export default defineConfig({
+  output: "server",
   vite: {
-    plugins: [
-      tailwindcss()
-    ]
+    plugins: [tailwindcss()],
   },
 
   integrations: [db()],
---
+++ src/middleware.ts
@@ -0,0 +1,20 @@
+import { auth } from "@/lib/auth";
+import { defineMiddleware } from "astro:middleware";
+
+export const onRequest = defineMiddleware(async (context, next) => {
+  const isAuthed = await auth.api.getSession({
+    headers: context.request.headers,
+  });
+
+  if (isAuthed) {
+    context.locals.user = isAuthed.user;
+    context.locals.session = isAuthed.session;
+  } else {
+    context.locals.user = null;
+    context.locals.session = null;
+  }
+  if (context.url.pathname === "/admin" && !isAuthed) {
+    return context.redirect("/login");
+  }
+  return next();
+});
