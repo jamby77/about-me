4a1cbfa637df74a769f054ce48b53166f7c7e290
jamby77 <jamby77@gmail.com>
2025-11-09 02:24:56 +0200

    add vercel integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,16 @@ import tailwindcss from "@tailwindcss/vite";
 
 import db from "@astrojs/db";
 
+import vercel from "@astrojs/vercel";
+
 // https://astro.build/config
 export default defineConfig({
   output: "server",
+
   vite: {
     plugins: [tailwindcss()],
   },
 
   integrations: [db()],
+  adapter: vercel(),
 });
